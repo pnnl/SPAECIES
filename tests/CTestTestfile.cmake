c3e56d73e078b7d49f449b230f7712e0fc31806c
Dong <dong9@nh-9917451.the-lab.llnl.gov>
2023-11-22 13:30:43 -0800

    removing superfluous files pt.2...

--- tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dong9/SPAECIES/tests
-# Build directory: /Users/dong9/SPAECIES/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/dong9/SPAECIES/tests/tests_include-b12d07c.cmake")
