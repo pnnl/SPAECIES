3a1472a989b32d9f4e224df433c3a234cb25d924
Dong <dong9@nh-9917451.the-lab.llnl.gov>
2023-11-22 13:26:52 -0800

    cleaning up superfluous cmake files...

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dong9/SPAECIES
-# Build directory: /Users/dong9/SPAECIES
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libspaecies")
-subdirs("rainshaft")
-subdirs("tests")
