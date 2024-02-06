9ddef098f11132d0dae8595f5ec97f91d9fd4e5d
Steven Roberts <roberts115@llnl.gov>
2024-02-05 16:56:22 -0800

    Adjust ERK controller to best settings

--- rainshaft/explicit_integrator.cpp
+++ rainshaft/explicit_integrator.cpp
@@ -1,6 +1,7 @@
 #include "explicit_integrator.hpp"
 #include "arkode/arkode_erkstep.h"
 #include "nvector/nvector_serial.h"
+#include "sunadaptcontroller/sunadaptcontroller_soderlind.h"
 
 ExplicitIntegrator::ExplicitIntegrator(const RainshaftConstants* constants,
                                        const RainshaftGrid* grid,
@@ -18,7 +19,19 @@ RainshaftSolution ExplicitIntegrator::integrate(double initial_time,
   auto y = state_to_n_vector(sun_ctxt, initial_state);
   void* arkode_mem = ERKStepCreate(rainshaft_f, initial_time, y, sun_ctxt);
   ERKStepSetUserData(arkode_mem, (void*) &user_data);
-  ERKStepSetMaxNumSteps(arkode_mem, -1);
+  ERKStepSetMaxNumSteps(arkode_mem, -1); // Set no limit on the number of steps
+
+  // Gustafsson’s explicit controller handles stability limited stepsizes well
+  auto controller = SUNAdaptController_ExpGus(sun_ctxt);
+  // Use no bias and instead rely on a safety factor. Shampine uses these values in the MATLAB ODE suite
+  SUNAdaptController_SetErrorBias(controller, 1);
+  ERKStepSetSafetyFactor(arkode_mem, 0.8);
+  // ARKODE currently approximates the optimal step size with (err)^(-1/(embedded order)), but it's more common to use
+  // (err)^(-1/(min(embedded order, order) + 1)). This adjusts the exponent to use the latter.
+  ERKStepSetAdaptivityAdjustment(arkode_mem, 0);
+  ERKStepSetFixedStepBounds(arkode_mem, 1, 1); // Remove deadzone
+
+
   const sunrealtype fac = 1.;
   const sunrealtype reltol = fac * 1.e-2;
   auto abstol = N_VClone(y);
@@ -50,6 +63,7 @@ RainshaftSolution ExplicitIntegrator::integrate(double initial_time,
   ERKStepGetNumRhsEvals(arkode_mem, &num_rhs_evals);
 
   N_VDestroy(y);
+  SUNAdaptController_Destroy(controller);
   // SPS: Make RAII wrapper for this.
   ERKStepFree(&arkode_mem);
   return RainshaftSolution(states, dvars, num_rhs_evals);
