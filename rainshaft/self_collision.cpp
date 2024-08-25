7fcf3691e51b2351a7d1266ffbffa22019beeaed
Steven Roberts <roberts115@llnl.gov>
2024-08-25 13:32:22 -0700

    Change derivative type for better compiler optimization

--- rainshaft/self_collision.cpp
+++ rainshaft/self_collision.cpp
@@ -1,9 +1,6 @@
 #include "self_collision.hpp"
 #include "derivatives.hpp"
-#include <cstddef>
-#include <cmath>
 #include <stdexcept>
-using std::min, std::cbrt, std::exp;
 
 RainshaftTendency SelfCollision::calc_tend(const RainshaftConstants &constants,
                                            const RainshaftGrid &grid,
@@ -14,7 +11,7 @@ RainshaftTendency SelfCollision::calc_tend(const RainshaftConstants &constants,
   std::vector<double> nr_tend(grid.nlev, 0.), qr_tend(grid.nlev, 0.);
   for (std::size_t il = 0; il != grid.nlev; ++il)
   {
-    const auto [b, b_grad] = breakup_fac(constants, state.nr[il], state.qr[il]);
+    const auto b = breakup_fac(constants, state.nr[il], state.qr[il]);
     nr_tend[il] = -5.78 * b * state.nr[il] * state.qr[il] * dvars.rho_dry[il];
   }
   return RainshaftTendency(t_tend, q_tend, nr_tend, qr_tend);
@@ -43,7 +40,7 @@ void SelfCollision::calc_tend_jac(const RainshaftConstants &constants,
     const auto i_nr = i_q + grid.nlev;
     const auto i_qr = i_nr + grid.nlev;
 
-    const auto [b, b_grad] = breakup_fac(constants, state.nr[il], state.qr[il]);
+    const auto [b, b_grad] = breakup_fac<true>(constants, state.nr[il], state.qr[il]);
     const auto [db_dnr, db_dqr] = b_grad;
 
     const auto [dr_dt, dr_dq] = dvars.rho_dry_grad(constants, state, il);
@@ -52,21 +49,5 @@ void SelfCollision::calc_tend_jac(const RainshaftConstants &constants,
     jac(i_nr, i_q) += -5.78 * b * state.nr[il] * state.qr[il] * dr_dq;
     jac(i_nr, i_nr) += -5.78 * state.qr[il] * dvars.rho_dry[il] * (db_dnr * state.nr[il] + b);
     jac(i_nr, i_qr) += -5.78 * state.nr[il] * dvars.rho_dry[il] * (db_dqr * state.qr[il] + b);
-  }
-}
-
-ValGrad<2> SelfCollision::breakup_fac(const RainshaftConstants &constants,
-                                  double nr, double qr) const
-{
-  const auto mean_mass_diam = std::cbrt(qr / (constants.pi * constants.rhow * nr));
-  const auto breakup_exp_term = std::exp(2300. * (2.8e-4 - mean_mass_diam));
-  const auto breakup = 2. - breakup_exp_term;
-  if (breakup <= 1) {
-    return {breakup, {
-      -2300. * breakup_exp_term * mean_mass_diam / (3. * nr),
-      2300. * breakup_exp_term * mean_mass_diam / (3. * qr)
-    }};
-  } else {
-    return {1., {0, 0}};
   }
 }
--- rainshaft/self_collision.hpp
+++ rainshaft/self_collision.hpp
@@ -1,10 +1,42 @@
 #ifndef SELF_COLLISION_HPP
 #define SELF_COLLISION_HPP
 #include <vector>
+#include <cmath>
 
 #include "rainshaft_process.hpp"
 
 class SelfCollision : public RainshaftProcess {
+
+private:
+  // For given rain variables, measure whether collisions typically end
+  // up merging drops (breakup_fac \approx 1) or is there significant
+  // breakup (breakup_fac < 1).
+  // Should not be called if nr is near 0.
+  template<bool WithGrad = false>
+  auto breakup_fac(const RainshaftConstants& constants,
+                     const double nr, const double qr) const
+  {
+    const auto mean_mass_diam = std::cbrt(qr / (constants.pi * constants.rhow * nr));
+    const auto breakup_exp_term = std::exp(2300. * (2.8e-4 - mean_mass_diam));
+    const auto breakup = 2. - breakup_exp_term;
+
+    if constexpr (WithGrad) {
+      if (breakup <= 1.0) {
+        return ValGrad<2>{
+          breakup,
+          {
+            -2300. * breakup_exp_term * mean_mass_diam / (3. * nr),
+            2300. * breakup_exp_term * mean_mass_diam / (3. * qr)
+          }
+        };
+      } else {
+        return ValGrad<2>{1.0, {0.0, 0.0}};
+      }
+    } else {
+      return std::min(1.0, breakup);
+    }
+  }
+
 
 public:
 
@@ -26,13 +58,6 @@ public:
                              const RainshaftState &state,
                              const RainshaftDerivedVars &dvars,
                              Matrix jac) const;
-
-  // For given rain variables, measure whether collisions typically end
-  // up merging drops (breakup_fac \approx 1) or is there significant
-  // breakup (breakup_fac < 1).
-  // Should not be called if nr is near 0.
-  ValGrad<2> breakup_fac(const RainshaftConstants& constants,
-                     double nr, double qr) const;
 
 };
 
