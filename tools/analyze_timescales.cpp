f0ddee9780cb18c1e0f2a4039227418d5eec5ee7
Chris Vogl <vogl2@llnl.gov>
2026-07-13 16:38:30 -0700

    fixup noting the addition of outputing and reading boundary conditions

--- tools/analyze_timescales.cpp
+++ tools/analyze_timescales.cpp
@@ -22,21 +22,34 @@ extern "C"
 #include "netcdf.h"
 }
 
+class NetcdfFile
+{
+  int file_id;
+  const std::string file_name;
+public:
+  NetcdfFile(const std::string& file_name);
+  ~NetcdfFile();
+  NetcdfFile(const NetcdfFile&) = delete;
+  NetcdfFile(NetcdfFile&&) = delete;
+  NetcdfFile& operator=(const NetcdfFile&) = delete;
+  NetcdfFile& operator=(NetcdfFile&&) = delete;
+  // CJV: revisit whether this should be const given the underlying file stream
+  //      is likely changed (even if the overall operation is read-only)
+  int get_file_id() const { return file_id; }
+  int get_dim_id(const std::string& dim_name) const;
+  int get_var_id(const std::string& var_name) const;
+};
 
 // variant NetcdfReader class modified for output of NetcdfWriter class
 class NetcdfReader
 {
-  int ncid;
+  NetcdfFile file;
 public:
-  NetcdfReader(const std::string& file_name);
-  ~NetcdfReader();
-  NetcdfReader(const NetcdfReader&) = delete;
-  NetcdfReader(NetcdfReader&&) = delete;
-  NetcdfReader& operator=(const NetcdfReader&) = delete;
-  NetcdfReader& operator=(NetcdfReader&&) = delete;
+  NetcdfReader(const std::string& file_name) : file(file_name) {}
   std::tuple<std::size_t, std::size_t> read_num_cases_and_levs() const;
-  std::vector<double> read_grid(std::size_t case_idx) const;
-  void read_state(std::size_t case_idx, State &initial_state, std::size_t time_idx) const;
+  std::vector<double> read_grid(const std::size_t case_idx) const;
+  void read_boundary_conditions(const std::size_t case_idx, RainshaftConstants &constants) const;
+  void read_state(const std::size_t case_idx, const std::size_t time_idx, State &initial_state) const;
 };
 
 
@@ -89,8 +102,9 @@ int main(int argc, char* argv[])
   for (std::size_t icase=0; icase < num_cases; icase++)
   {
 
-    reader.read_state(icase, state, time_idx);
+    reader.read_boundary_conditions(icase, constants);
     RainshaftGrid grid = reader.read_grid(icase);
+    reader.read_state(icase, time_idx, state);
     RainshaftDerivedVars dvars(constants, grid, state, defaults::regularize_lambdar);
 
     evap.calc_tend(constants, grid, state, dvars, evap_tend);
@@ -107,6 +121,9 @@ int main(int argc, char* argv[])
         qr[ilev] / (evap_tend.get_variable("qr_tend").value()[ilev] + epsilon));
       self_coll_rate = std::max(self_coll_rate,
         nr[ilev] / (self_coll_tend.get_variable("nr_tend").value()[ilev] + epsilon));
+      
+      
+
       }
     std::cout << "Evaporation: " << evaporation_rate << std::endl;
     std::cout << "Self-collection: " << self_coll_rate << std::endl;
@@ -115,59 +132,87 @@ int main(int argc, char* argv[])
   return 0;
 }
 
-NetcdfReader::NetcdfReader(const std::string& file_name)
+NetcdfFile::NetcdfFile(const std::string& file_name) : file_name(file_name)
 {
-  nc_open(file_name.c_str(), NC_NOWRITE, &ncid);
+  // CJV: check if there should be an error check here
+  nc_open(file_name.c_str(), NC_NOWRITE, &file_id);
 }
 
-NetcdfReader::~NetcdfReader()
+NetcdfFile::~NetcdfFile()
 {
-  nc_close(ncid);
+  nc_close(file_id);
+}
+
+int NetcdfFile::get_dim_id(const std::string& dim_name) const
+{
+  int dim_id;
+  if (nc_inq_dimid(file_id, dim_name.c_str(), &dim_id) == NC_ENOTVAR)
+  {
+    throw std::runtime_error("NetCDF file " + file_name + " does not contain dimension " + dim_name);
+  }
+  return dim_id;
+}
+
+int NetcdfFile::get_var_id(const std::string& var_name) const
+{
+  int var_id;
+  if (nc_inq_varid(file_id, var_name.c_str(), &var_id) == NC_ENOTVAR)
+  {
+    throw std::runtime_error("NetCDF file " + file_name + " does not contain variable " + var_name);
+  }
+  return var_id;
 }
 
 std::tuple<std::size_t, std::size_t> NetcdfReader::read_num_cases_and_levs() const
 {
-  int case_id, lev_id;
-  nc_inq_dimid(ncid, "case", &case_id);
-  nc_inq_dimid(ncid, "lev", &lev_id);
+  const int case_id = file.get_dim_id("case");
+  const int lev_id = file.get_dim_id("lev");
   std::size_t num_cases, num_levs;
-  nc_inq_dimlen(ncid, case_id, &num_cases);
-  nc_inq_dimlen(ncid, lev_id, &num_levs);
+  nc_inq_dimlen(file.get_file_id(), case_id, &num_cases);
+  nc_inq_dimlen(file.get_file_id(), lev_id, &num_levs);
   return {num_cases, num_levs};
 }
 
-std::vector<double> NetcdfReader::read_grid(std::size_t case_idx) const
+std::vector<double> NetcdfReader::read_grid(const std::size_t case_idx) const
 {
-  int ilev_id, z_int_id;
-  nc_inq_dimid(ncid, "ilev", &ilev_id);
-  nc_inq_varid(ncid, "z_int", &z_int_id);
+  const int ilev_id = file.get_dim_id("ilev");
+  const int z_int_id = file.get_var_id("z_int");
   std::size_t num_ilevs;
-  nc_inq_dimlen(ncid, ilev_id, &num_ilevs);
+  nc_inq_dimlen(file.get_file_id(), ilev_id, &num_ilevs);
   std::size_t starts[2] = {case_idx, 0};
   std::size_t counts[2] = {1, num_ilevs};
   std::vector<double> z_int(num_ilevs);
-  nc_get_vara_double(ncid, z_int_id, starts, counts, z_int.data());
+  nc_get_vara_double(file.get_file_id(), z_int_id, starts, counts, z_int.data());
   return std::move(z_int);
 }
 
-void NetcdfReader::read_state(std::size_t case_idx, State &state, std::size_t time_idx) const
+void NetcdfReader::read_boundary_conditions(const std::size_t case_idx, RainshaftConstants &constants) const
 {
-  int lev_id, T_id, q_id, nr_id, qr_id;
-  nc_inq_varid(ncid, "lev", &lev_id);
-  nc_inq_varid(ncid, "T", &T_id);
-  nc_inq_varid(ncid, "q", &q_id);
-  nc_inq_varid(ncid, "nr", &nr_id);
-  nc_inq_varid(ncid, "qr", &qr_id);
+  const int rho_top_id = file.get_var_id("rho_top");
+  const int qr_top_id = file.get_var_id("qr_top");
+  const int nr_top_id = file.get_var_id("nr_top");
+  nc_get_var1_double(file.get_file_id(), rho_top_id, &case_idx, &constants.rho_top);
+  nc_get_var1_double(file.get_file_id(), qr_top_id, &case_idx, &constants.qr_top);
+  nc_get_var1_double(file.get_file_id(), nr_top_id, &case_idx, &constants.nr_top);
+}
+
+void NetcdfReader::read_state(const std::size_t case_idx, const std::size_t time_idx, State &state) const
+{
+  const int lev_id = file.get_dim_id("lev");
+  const int T_id = file.get_var_id("T");
+  const int q_id = file.get_var_id("q");
+  const int nr_id = file.get_var_id("nr");
+  const int qr_id = file.get_var_id("qr");
   std::size_t num_levs;
-  nc_inq_dimlen(ncid, lev_id, &num_levs);
+  nc_inq_dimlen(file.get_file_id(), lev_id, &num_levs);
   std::size_t starts[3] = {case_idx, time_idx, 0};
   std::size_t counts[3] = {1, 1, num_levs};
   spaecies::ContiguousVariableView<double> T = state.get_variable("T").value();
   spaecies::ContiguousVariableView<double> q = state.get_variable("q").value();
   spaecies::ContiguousVariableView<double> nr = state.get_variable("nr").value();
   spaecies::ContiguousVariableView<double> qr = state.get_variable("qr").value();
-  nc_get_vara_double(ncid, T_id, starts, counts, &T[0]);
-  nc_get_vara_double(ncid, q_id, starts, counts, &q[0]);
-  nc_get_vara_double(ncid, nr_id, starts, counts, &nr[0]);
-  nc_get_vara_double(ncid, qr_id, starts, counts, &qr[0]);
+  nc_get_vara_double(file.get_file_id(), T_id, starts, counts, &T[0]);
+  nc_get_vara_double(file.get_file_id(), q_id, starts, counts, &q[0]);
+  nc_get_vara_double(file.get_file_id(), nr_id, starts, counts, &nr[0]);
+  nc_get_vara_double(file.get_file_id(), qr_id, starts, counts, &qr[0]);
 }
